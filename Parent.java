1180603f8c9ab9b2e9785ab96b2e0d26485a0c49
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-03-02 00:23:13 +0530

    Create Parent.java

---
+++ Parent.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	String name="G Pavan Shanmukha Madhav";
+	int rollno=22049;
+	int age=19;
+	 
+	public static void main(String[] args)
+	{
+		Main obj=new Main();
+		System.out.println(obj.name);
+		System.out.println(obj.rollno);
+		System.out.println(obj.age);
+	}
+}
