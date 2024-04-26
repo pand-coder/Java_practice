0d3f5b8641a0acee2ab2b4852873a5d17b15f624
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-04-27 00:02:55 +0530

    Create arx.java

---
+++ arx.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class Search{
+    public void linearsearch(n,arr)
+    {
+        for
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        do{
+        System.out.println("Enter the size of the array:");
+        n=sc.nextInt();
+        int arr[n];
+        for(int i=1;i,n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("MENU DRIVEN ARRAY SORT- SEARCH PROGRAM");
+        System.out.println("ALGORITHM       -   Time Complexity");
+        System.out.println("1.) Linear Search - O(n)");
+        System.out.println("2.) Binary Search - O(logn)");
+        System.out.println("3.) Bubble sort - O(n^2)");
+        System.out.println("4.) Selection sort - O(n^2)");
+        System.out.println("5.) Insertion Sort - O(n^2)");
+        System.out.println("6.) Merge Sort - O(n logn)");
+        System.out.println("7.) Radix sort - O(nk)");
+        
+        int choice;
+        
+        choice=sc.nextInt();
+        switch(choice)
+        {
+            case 1:
+                System.out.println("Searching Algorithm : Linear Search")
+                
+                
+        }
+        
+        }
+
+        
+        
+        
+    }
+}
